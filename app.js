e93587d8bdb74d394f893daa2ba93c7814385c44
Ryan <ryanpiveteau@gmail.com>
2017-09-05 16:38:33 -0400

    versio num

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var connection = mysql.createConnection({
 var loachannel = connectFile.loachannel;
 var loaDays = [6, 0];
 
-var versionNum = '4.0.7';
+var versionNum = '4.0.8';
 var versionAnnounce = 0;
 
 var sampleDate = moment().format('MM/DD/YY');
